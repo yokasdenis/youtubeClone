b0855891df5148f0956fb7f32086b8e0398ced9b
yokasdenis <yokasdenis@hotmail.com>
2023-12-07 19:36:55 +0300

    second commit

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ topMainBody.addEventListener('scroll', function () {
 }});
 
 
-const API_KEY="AIzaSyDADh9Tks_KcpyjmnyqDLAiIrvpjW8QVAo";
+const API_KEY="AIzaSyAyBVobRZdsqv8hb0R5w6pRAds5NWAw1Zo";
 const BASE_URL = "https://www.googleapis.com/youtube/v3";
 
 
@@ -30,38 +30,42 @@ async function fetchVideos(searchQuery, maxResults) {
   const data = await response.json();
   localStorage.setItem("data",JSON.stringify(data.items))
 }
-fetchVideos("music",15);
 
 
-// async function getChannelLogo(channelId){
-//   // https://www.googleapis.com/youtube/v3/channels?key=AIzaSyBmOfUnRNYc22e04ZmK79uRbPb6388K9AE&part=snippet&id=UC8Wd_RVw8T1O1_IWEbICkIg
-//   const response = await fetch(`${BASE_URL}/channels?key=${API_KEY}&part=snippet&id=${channelId}`);
-//   const data = await response.json();
-//   // console.log(data);
-//   localStorage.setItem("data2",JSON.stringify(data.items))
-// }
+
+async function getChannelLogo(channelId){
+  // https://www.googleapis.com/youtube/v3/channels?key=AIzaSyBmOfUnRNYc22e04ZmK79uRbPb6388K9AE&part=snippet&id=UC8Wd_RVw8T1O1_IWEbICkIg
+  const response = await fetch(`${BASE_URL}/channels?key=${API_KEY}&part=snippet&id=${channelId}`);
+  const data = await response.json();
+  // console.log(data);
+  localStorage.setItem("data2",JSON.stringify(data))
+}
+
 async function getVideoStats(videoId){
   // https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBmOfUnRNYc22e04ZmK79uRbPb6388K9AE&part=statistics&id=JhIBqykjzbs
   const response = await fetch(`${BASE_URL}/videos?key=${API_KEY}&part=statistics&id=${videoId}`);
   const data = await response.json();
-  console.log(data);
-  localStorage.setItem("data3",JSON.stringify(data.item))
+  // console.log(data);
+  localStorage.setItem("data3",JSON.stringify(data))
+  // displayView(data.items[0].statistics.viewCount)
 }
 
 let dta=JSON.parse(localStorage.getItem("data"));
 console.log(dta);
-// let dta2=JSON.parse(localStorage.getItem("data2"));
-let dta3=JSON.parse(localStorage.getItem("data3"));
-// console.log(dta3);
+
+
 let divIds=[];
+let divLogo=[];
 
 function renderAll(){
-
+ 
   for (let i = 0; i < dta.length; i++) {
-    // getChannelLogo(dta[i].snippet.channelId)
-    // console.log(dta2);
-    // getVideoStats(String(dta[i].id.videoId))
-    // console.log(dta3);
+    getChannelLogo(dta[i].snippet.channelId)
+    let dta2=JSON.parse(localStorage.getItem("data2"));
+    getVideoStats(dta[i].id.videoId)
+    let dta3=JSON.parse(localStorage.getItem("data3"));
+    // console.log(dta3.items[0].statistics.viewCount);
+    // console.log(dta2.items[0].snippet.thumbnails.default.url);
     let currentDate = new Date();
     const card = document.createElement("div");
     card.classList.add("card");
@@ -69,23 +73,61 @@ function renderAll(){
       <div id="${dta[i].id.videoId}">
       </div>
       <div>
-        ${dta[i].snippet.description}
-        ${dta[i].snippet.channelTitle}
-        ${calcTime(currentDate, new Date(dta[i].snippet.publishedAt))}
+      <img src=${dta2.items[0].snippet.thumbnails.default.url}>
+      <div>
+        <span>${dta[i].snippet.description}</span>
+        <span>${dta[i].snippet.channelTitle}</span>
+        <span>${getViews(dta3.items[0].statistics.viewCount)}</span>
+        <span>${calcTime(currentDate, new Date(dta[i].snippet.publishedAt))}</span>
+        </div>
       </div>
     `;
 
     cardContainer.appendChild(card); // Append the card to the container
 
    divIds.push(dta[i].id.videoId)
+ 
   }
 }
+function getViews(n){
+  if(n < 1000)
+      return n ;
 
+  if(n <= 999999){
+      n /= 1000;
+      n = parseInt(n);
+      return n + "K" ;
+  }
+  return parseInt(n / 1000000).toFixed(1) + "M" ;
+}
 
+// function displayView(data){
+//     const spanDiv=document.createElement("span");
+//  let views=getViews(data)
+//     span.innerText=views;
+//   const lastDivInCard = document.getElementById("test");
+//   lastDivInCard.appendChild(spanDiv);
+// }
+
+const searchInput=document.getElementById("search");
+const searchButton=document.getElementById("searchIconDiv")
+// searchButton.addEventListener("click",searchFun);
+// function searchFun() {
+//   cardContainer.innerHTML="";
+//   let searchVal=searchInput.value
+//   // if(searchVal==""){
+//   //   fetchVideos("music",12)
+//   //   re
+//   // }else{
+//   //   fetchVideos(searchVal,12)
+//   // }
+// }
+// renderAll();
 window.addEventListener("load", () => {
   // we need to write logic for rendering video player
   // iframe
-  renderAll();
+
+  renderAll()
   divIds.forEach((item) => {
     let videoId =String(item);
     // console.log(videoId);
@@ -115,19 +157,19 @@ function calcTime (curr,old) {
   var years = Math.floor(days / 365);
 
   if (years > 0) {
-    return years + (years === 1 ? " year" : " years");
+    return years + (years === 1 ? " year ago" : " years ago ");
   } else if (months > 0) {
-    return months + (months === 1 ? " month" : " months");
+    return months + (months === 1 ? " month ago" : " months ago");
   } else if (weeks > 0) {
-    return weeks + (weeks === 1 ? " week" : " weeks");
+    return weeks + (weeks === 1 ? " week ago" : " weeks ago");
   } else if (days > 0) {
-    return days + (days === 1 ? " day" : " days");
+    return days + (days === 1 ? " day ago" : " days ago");
   } else if (hours > 0) {
-    return hours + (hours === 1 ? " hour" : " hours");
+    return hours + (hours === 1 ? " hour ago" : " hours ago");
   } else if (minutes > 0) {
-    return minutes + (minutes === 1 ? " minute" : " minutes");
+    return minutes + (minutes === 1 ? " minute ago" : " minutes ago");
   } else {
-    return seconds + (seconds === 1 ? " second" : " seconds");
+    return seconds + (seconds === 1 ? " second ago" : " seconds ago");
   }
 
 }
